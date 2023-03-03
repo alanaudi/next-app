01922e097c798cca5d049a3ecbdadf447c0931bf
Alan Chang <alanaudi.tw@gmail.com>
2023-03-03 10:17:22 +0800

    chore: use tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import styles from '@/styles/pages/home.module.scss'
 
 export default function Home() {
-	return <main className={styles.main}></main>
+	return <main className={styles.main}>Home Page</main>
 }
